d6c766488c39004203563198f29e1b44442534e9
adrieljob <thonyadriel@gmail.com>
2024-04-11 15:03:10 -0300

    Reapply "atualizaçoes"
    This reverts commit 376f6d038adaebe99e023a77178e23dbfcaf4ea3.

---
+++ exercicio windows forms/Form6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio_windows_forms
+{
+    public partial class Form6 : Form
+    {
+        public Form6(string nome, string cantor)
+        {
+            InitializeComponent();
+            labelNome.Text = "Nome: " + nome;
+            labelCantor.Text = "Cantor(a): " + cantor;
+
+        }
+
+        private void labelNome_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
